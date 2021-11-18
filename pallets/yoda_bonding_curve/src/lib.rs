a9cff823c4b7f96fef846c5debfdc0a88c9f9b37
loaki07 <loakesh.i@gmail.com>
2021-11-18 20:07:02 +0530

    add: Assets storagemap with asset_id for yoda_bonding_curve

--- pallets/yoda_bonding_curve/src/lib.rs
+++ pallets/yoda_bonding_curve/src/lib.rs
@@ -16,12 +16,13 @@ mod benchmarking;
 
 #[frame_support::pallet]
 pub mod pallet {
-	use frame_support::traits::{Currency, ExistenceRequirement, Randomness, Get};
+	use frame_support::inherent::Vec;
+	use frame_support::traits::{Currency, ExistenceRequirement, Get, Randomness};
+	use frame_support::PalletId;
 	use frame_support::{dispatch::DispatchResult, pallet_prelude::*};
 	use frame_system::pallet_prelude::*;
 	use orml_traits::{MultiCurrency, MultiReservableCurrency};
 	use scale_info::TypeInfo;
-	use frame_support::PalletId;
 	use sp_runtime::traits::{AccountIdConversion, SaturatedConversion};
 
 	type BalanceOf<T> =
@@ -31,7 +32,7 @@ pub mod pallet {
 		<T as frame_system::Config>::AccountId,
 	>>::CurrencyId;
 
-	#[derive(Encode, Decode, TypeInfo)]
+	#[derive(Encode, Decode, TypeInfo, Clone)]
 	#[scale_info(skip_type_params(T))]
 	#[cfg_attr(feature = "std", derive(Debug))]
 	pub struct BondingCurve<T: Config> {
@@ -39,17 +40,25 @@ pub mod pallet {
 		// account: AccountId,
 		/// The creator of the bonding curve.
 		creator: AccountOf<T>,
-		/// The currency id of the bonding curve token.
-		currency_id: CurrencyIdOf<T>,
+		/// The asset id of the bonding curve token.
+		asset_id: CurrencyIdOf<T>,
 		/// The exponent of the curve.
 		exponent: u32,
 		/// The slope of the curve.
 		slope: u128,
 		/// The maximum supply that can be minted from the curve.
 		max_supply: u128,
+		/// the token name
+		token_name: Vec<u8>,
+		/// The token symbol
+		token_symbol: Vec<u8>,
+		/// Token decimals
+		token_decimals: u8,
+		/// token bonding curve_id
+		curve_id: u64,
 	}
 
-	impl<T: Config> BondingCurve::<T> {
+	impl<T: Config> BondingCurve<T> {
 		/// Integral when the curve is at point `x`.
 		pub fn integral(&self, x: u128) -> u128 {
 			let nexp = self.exponent + 1;
@@ -81,72 +90,114 @@ pub mod pallet {
 
 	#[pallet::storage]
 	#[pallet::getter(fn curves)]
-	pub(super) type Curves<T: Config> = StorageMap<_, Twox64Concat, u64, Option<BondingCurve<T>>, ValueQuery>;
+	pub(super) type Curves<T: Config> =
+		StorageMap<_, Twox64Concat, u64, Option<BondingCurve<T>>, ValueQuery>;
 
 	#[pallet::storage]
 	#[pallet::getter(fn next_curve_id)]
 	pub(super) type NextCurveId<T: Config> = StorageValue<_, u64, ValueQuery>;
 
+	#[pallet::storage]
+	#[pallet::getter(fn assets)]
+	pub(super) type Assets<T: Config> =
+		StorageMap<_, Twox64Concat, CurrencyIdOf<T>, BondingCurve<T>, OptionQuery>;
+
+	#[pallet::storage]
+	#[pallet::getter(fn account)]
+	pub(super) type Account<T: Config> = StorageDoubleMap<
+		_,
+		Blake2_128Concat,
+		CurrencyIdOf<T>,
+		Blake2_128Concat,
+		T::AccountId,
+		BalanceOf<T>,
+		ValueQuery,
+	>;
+
 	#[pallet::event]
 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
 	pub enum Event<T: Config> {
 		/// (CurveId, Creator)
-        NewCurve(u64, AccountOf<T>),
-        /// (Buyer, CurveId, Amount, Cost)
-        CurveBuy(AccountOf<T>, u64, BalanceOf<T>, BalanceOf<T>),
-        /// (Seller, CurveId, Amount, Return)
-        CurveSell(AccountOf<T>, u64, BalanceOf<T>, BalanceOf<T>),
+		NewCurve(u64, AccountOf<T>),
+		/// (Buyer, CurveId, Amount, Cost)
+		CurveBuy(AccountOf<T>, u64, BalanceOf<T>, BalanceOf<T>),
+		/// (Seller, CurveId, Amount, Return)
+		CurveSell(AccountOf<T>, u64, BalanceOf<T>, BalanceOf<T>),
 	}
 
 	#[pallet::error]
 	pub enum Error<T> {
 		/// Sender does not have enough base currency to reserve for a new curve.
-        InsufficientBalanceToReserve,
-        /// A curve does not exist for this curve id.
-        CurveDoesNotExist,
-        /// Sender does not have enough base currency to make a purchase.
-        InsufficentBalanceForPurchase,
-        /// The currency that is trying to be created already exists.
-        CurrencyAlreadyExists,
+		InsufficientBalanceToReserve,
+		/// A curve does not exist for this curve id.
+		CurveDoesNotExist,
+		/// Sender does not have enough base currency to make a purchase.
+		InsufficentBalanceForPurchase,
+		/// The currency that is trying to be created already exists.
+		CurrencyAlreadyExists,
 		/// Error while using deposit from orml currencies
 		ErrorWhileDeposit,
 		/// Error while using ensure_withdraw from orml currencies
-		ErrorWhileWithdraw
+		ErrorWhileWithdraw,
+		/// Error when an asset does not exist
+		AssetDoesNotExist,
 	}
-
 
 	#[pallet::call]
 	impl<T: Config> Pallet<T> {
 		#[pallet::weight(0 + T::DbWeight::get().writes(1))]
-		pub fn create(origin: OriginFor<T>, currency_id: CurrencyIdOf<T>, exponent: u32, slope: u128, max_supply: u128) -> DispatchResult {
+		pub fn create(
+			origin: OriginFor<T>,
+			asset_id: CurrencyIdOf<T>,
+			exponent: u32,
+			slope: u128,
+			max_supply: u128,
+			token_name: Vec<u8>,
+			token_symbol: Vec<u8>,
+			token_decimals: u8,
+		) -> DispatchResult {
 			let sender = ensure_signed(origin)?;
 
 			// Requires an amount to be reserved.
-            ensure!(
-                T::Currency::can_reserve(T::GetNativeCurrencyId::get(), &sender, T::CurveDeposit::get()),
-                Error::<T>::InsufficientBalanceToReserve,
-            );
-
+			ensure!(
+				T::Currency::can_reserve(
+					T::GetNativeCurrencyId::get(),
+					&sender,
+					T::CurveDeposit::get()
+				),
+				Error::<T>::InsufficientBalanceToReserve,
+			);
 			// Ensure that a curve with this id does not already exist.
-            ensure!(
-                T::Currency::total_issuance(currency_id) == 0u32.into(),
-                Error::<T>::CurrencyAlreadyExists,
-            );
+
+			ensure!(
+				T::Currency::total_issuance(asset_id) == 0u32.into(),
+				Error::<T>::CurrencyAlreadyExists,
+			);
 
 			// Adds 1 of the token to the module account.
-            T::Currency::deposit(currency_id, &T::PalletId::get().into_account(), 1u32.saturated_into()).map_err(|e| <Error<T>>::ErrorWhileWithdraw)?;
+			T::Currency::deposit(
+				asset_id,
+				&T::PalletId::get().into_account(),
+				1u32.saturated_into(),
+			)
+			.map_err(|_e| <Error<T>>::ErrorWhileWithdraw)?;
+			let curve_id = Self::next_id();
 
-			 let new_curve = BondingCurve::<T> {
-                creator: sender.clone(),
-                currency_id,
-                exponent,
-                slope,
-                max_supply,
-            };
+			let new_curve = BondingCurve::<T> {
+				creator: sender.clone(),
+				asset_id,
+				exponent,
+				slope,
+				max_supply,
+				token_name,
+				token_symbol,
+				token_decimals,
+				curve_id,
+			};
 
 			// Mutations start here
-            let curve_id = Self::next_id();
-            <Curves<T>>::insert(curve_id, Some(new_curve));
+			<Curves<T>>::insert(curve_id.clone(), Some(new_curve.clone()));
+			<Assets<T>>::insert(asset_id.clone(), new_curve);
 
 			Self::deposit_event(Event::NewCurve(curve_id, sender));
 
@@ -154,78 +205,97 @@ pub mod pallet {
 		}
 
 		#[pallet::weight(0 + T::DbWeight::get().writes(1))]
-		pub fn buy(origin: OriginFor<T>, curve_id: u64, amount: BalanceOf<T>) -> DispatchResult {
+		pub fn buy(
+			origin: OriginFor<T>,
+			asset_id: CurrencyIdOf<T>,
+			amount: BalanceOf<T>,
+		) -> DispatchResult {
 			let sender = ensure_signed(origin)?;
-			
-			if let Some(curve) = Self::curves(curve_id) {
-                let currency_id = curve.currency_id;
-                let total_issuance = T::Currency::total_issuance(currency_id).saturated_into::<u128>();
-                let issuance_after = total_issuance + amount.saturated_into::<u128>();
 
-                ensure!(
-                    issuance_after <= curve.max_supply,
-                    "Exceeded max supply.",
-                );
+			if let Some(asset) = Self::assets(asset_id) {
+				let asset_id = asset.asset_id;
+				let total_issuance = T::Currency::total_issuance(asset_id).saturated_into::<u128>();
+				let issuance_after = total_issuance + amount.saturated_into::<u128>();
 
-                let integral_before: BalanceOf<T> = curve.integral(total_issuance).saturated_into();
-                let integral_after: BalanceOf<T> = curve.integral(issuance_after).saturated_into();
-                
-                let cost = integral_after - integral_before;
-                ensure!(
-                    T::Currency::free_balance(T::GetNativeCurrencyId::get(), &sender) >= cost.into(),
-                    Error::<T>::InsufficentBalanceForPurchase,
-                );
-				let curve_account = T::PalletId::get().into_sub_account(curve_id);
+				ensure!(issuance_after <= asset.max_supply, "Exceeded max supply.",);
 
-                T::Currency::transfer(T::GetNativeCurrencyId::get(), &sender, &curve_account, cost).ok(); // <- Why does the `?` operator not work?
+				let integral_before: BalanceOf<T> = asset.integral(total_issuance).saturated_into();
+				let integral_after: BalanceOf<T> = asset.integral(issuance_after).saturated_into();
 
-                T::Currency::deposit(currency_id, &sender, amount).ok();
+				let cost = integral_after - integral_before;
+				ensure!(
+					T::Currency::free_balance(T::GetNativeCurrencyId::get(), &sender)
+						>= cost.into(),
+					Error::<T>::InsufficentBalanceForPurchase,
+				);
+				let curve_account = T::PalletId::get().into_sub_account(asset.curve_id);
 
-                Self::deposit_event(Event::CurveBuy(sender, curve_id, amount, cost));
+				T::Currency::transfer(T::GetNativeCurrencyId::get(), &sender, &curve_account, cost)
+					.ok(); // <- Why does the `?` operator not work?
+
+				T::Currency::deposit(asset_id, &sender, amount).ok();
+
+				Self::deposit_event(Event::CurveBuy(sender, asset.curve_id, amount, cost));
 				Ok(())
-            } else {
-                Err(Error::<T>::CurveDoesNotExist)?
-            }
+			} else {
+				Err(Error::<T>::CurveDoesNotExist)?
+			}
 		}
 
 		#[pallet::weight(0 + T::DbWeight::get().reads_writes(1,1))]
-		pub fn sell(origin: OriginFor<T>, curve_id: u64, amount: BalanceOf<T>) -> DispatchResult {
+		pub fn sell(
+			origin: OriginFor<T>,
+			asset_id: CurrencyIdOf<T>,
+			amount: BalanceOf<T>,
+		) -> DispatchResult {
 			let sender = ensure_signed(origin)?;
 
-			  if let Some(curve) = Self::curves(curve_id) {
-                let currency_id = curve.currency_id;
+			if let Some(asset) = Self::assets(asset_id) {
+				let asset_id = asset.asset_id;
 
-                T::Currency::ensure_can_withdraw(currency_id, &sender, amount).map_err(|e| <Error<T>>::ErrorWhileWithdraw)?;
-                
-                let total_issuance = T::Currency::total_issuance(currency_id);
-                let issuance_after = total_issuance - amount;
+				T::Currency::ensure_can_withdraw(asset_id, &sender, amount)
+					.map_err(|_e| <Error<T>>::ErrorWhileWithdraw)?;
 
-                let integral_before: BalanceOf<T> = curve.integral(total_issuance.saturated_into::<u128>()).saturated_into();
-                let integral_after: BalanceOf<T> = curve.integral(issuance_after.saturated_into::<u128>()).saturated_into();
+				let total_issuance = T::Currency::total_issuance(asset_id);
+				let issuance_after = total_issuance - amount;
 
-                let return_amount = integral_before - integral_after;
+				let integral_before: BalanceOf<T> =
+					asset.integral(total_issuance.saturated_into::<u128>()).saturated_into();
+				let integral_after: BalanceOf<T> =
+					asset.integral(issuance_after.saturated_into::<u128>()).saturated_into();
 
-				let curve_account = T::PalletId::get().into_sub_account(curve_id);
+				let return_amount = integral_before - integral_after;
+				let curve_account = T::PalletId::get().into_sub_account(asset.curve_id);
 
-                T::Currency::withdraw(currency_id, &sender, amount).ok();
+				T::Currency::withdraw(asset_id, &sender, amount).ok();
 
-                T::Currency::transfer(T::GetNativeCurrencyId::get(), &curve_account, &sender, return_amount).ok();
+				T::Currency::transfer(
+					T::GetNativeCurrencyId::get(),
+					&curve_account,
+					&sender,
+					return_amount,
+				)
+				.ok();
 
-                Self::deposit_event(Event::CurveSell(sender, curve_id, amount, return_amount));
+				Self::deposit_event(Event::CurveSell(
+					sender,
+					asset.curve_id,
+					amount,
+					return_amount,
+				));
 				Ok(())
-            } else {
-                Err(Error::<T>::CurveDoesNotExist)?
-            }
+			} else {
+				Err(Error::<T>::CurveDoesNotExist)?
+			}
 		}
 	}
 }
 
-
-impl<T: Config> Pallet<T> { 
-    /// DANGER - Mutates storage
-    fn next_id() -> u64 {
-        let id = Self::next_curve_id();
-        <NextCurveId<T>>::mutate(|n| *n += 1);
-        id
-    }
+impl<T: Config> Pallet<T> {
+	/// DANGER - Mutates storage
+	fn next_id() -> u64 {
+		let id = Self::next_curve_id();
+		<NextCurveId<T>>::mutate(|n| *n += 1);
+		id
+	}
 }
