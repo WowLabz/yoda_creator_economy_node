d1258192d3cbdf10f6a889955355000ad4c40f30
Varun <varun24143@gmail.com>
2021-12-08 18:41:19 +0530

    wip: bancor curve implementations

--- pallets/yoda_bonding_curve/src/curves.rs
+++ pallets/yoda_bonding_curve/src/curves.rs
@@ -1,6 +1,6 @@
 use frame_support::pallet_prelude::*;
 use scale_info::TypeInfo;
-use sp_runtime::SaturatedConversion;
+use sp_runtime::{SaturatedConversion, traits::Scale};
 
 pub trait CurveConfig {
 	fn integral_before(&self, issuance: u128) -> u128;
@@ -79,3 +79,73 @@ impl CurveConfig for Linear {
 		self.integral(issuance).saturated_into()
 	}
 }
+
+const MAX_RESERVE_RATIO: u128 = 1000000;
+
+pub struct Power {
+	base_N: u128,
+	base_D: u128,
+	exp_N: u128,
+	exp_D: u128,
+}
+pub struct CalculatePurchaseAndSellReturn {
+	pub supply: u128,
+	pub reserve_balance: u128,
+	pub reserve_ratio: u128,
+	pub deposit_amount: u128,
+	pub power: Power,
+}
+
+impl CalculatePurchaseAndSellReturn {
+
+	pub fn integral_purchase(&self, precision: u128, base_N: u128) -> u128 {
+		let power: u128 = (base_N / self.power.base_D)
+		.pow(self.power.exp_N as u32 / self.power.exp_D as u32)*2;
+		let power_with_precision = power.pow(precision as u32);
+		return power_with_precision
+	}
+
+	pub fn purchase_return(self, 
+		supply: u128, reserve_balance: u128,
+		reserve_ratio: u128, deposit_amount: u128) -> u128 {
+		assert!(supply > 0 && 
+		reserve_balance > 0 && 
+		reserve_ratio > 0 && 
+		reserve_ratio <= MAX_RESERVE_RATIO);
+
+		if deposit_amount == 0 {
+			
+			return 0
+		} 
+		else {
+			
+			let result: u128 = supply.mul(deposit_amount).div(reserve_balance);
+			let precision: u128 = 10;
+			let base_N: u128 = deposit_amount + reserve_balance;
+			let value: u128 = self.integral_purchase(precision, base_N);
+			let new_token_supply: u128 = supply.mul(value) >> precision;
+			return new_token_supply - supply
+
+		}
+	}
+
+	pub fn calculate_sale_return(self,
+		supply: u128, reserve_balance: u128,
+		reserve_ratio: u128, sell_amount: u128) -> u128 {
+			assert!(supply > 0 && 
+				reserve_balance > 0 && 
+				reserve_ratio > 0 && 
+				reserve_ratio <= MAX_RESERVE_RATIO);
+				if sell_amount == 0 {
+			
+					return 0
+				} else {
+					let result: u128 = reserve_balance.mul(sell_amount).div(supply);
+					let precision: u128 = 10;
+					let old_balance: u128 = reserve_balance.mul(result);
+					let new_balance: u128 = reserve_balance << precision;
+					return (old_balance - new_balance).div(result)
+				}	
+
+}
+} 
